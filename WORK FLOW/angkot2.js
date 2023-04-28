10d2a851d7529a81ce32b8fcda576bdab351b09d
ZKYRIO <hamdanioke1512@gmail.com>
2023-04-28 21:47:41 +0700

    latihan js kedua (for  loop)

---
+++ WORK FLOW/angkot2.js
@@ -0,0 +1,13 @@
+let jmlhAngkot = 10;
+let angkotBeroperasi = 8;
+let noAngkot = 1;
+while(noAngkot <= angkotBeroperasi) {
+    console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik.')
+    noAngkot++;
+}
+
+// alert(noAngkot)
+
+for( noAngkot ; noAngkot <= jmlhAngkot ; noAngkot++) {
+    console.log('Angkot No. ' + noAngkot + ' tidak beroperasi.')
+}
